0d3823e15ea17cbe33e6a25795fb300f965c95eb
JimYeh <yamap52088@gmail.com>
2018-06-20 15:57:09 +0800

    ✨ Feat: Add profile list in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,78 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        f2e-challenge
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+.container
+  h1.title F2E Chanlleng
+  ul.profile
+    li.profile__list(
+      v-for="profile in profileList",
+      :key="profile.title"
+    )
+      a(:href="profile.link")
+        .profile__list__img
+          img(:src="profile.image")
+        .profile__list__title
+          p(v-text="profile.title")
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 
 export default {
+  data() {
+    return {
+      profileList: [
+        {
+          title: 'Week 1 - Todo List',
+          image: require('~/static/f2e-challenge-todo.png'),
+          link: 'https://jimyah.github.io/vue-todo/'
+        },
+        {
+          title: 'Week 1 - Filter',
+          image: require('~/static/f2e-challenge-filter.png'),
+          link: '/travel'
+        }
+      ]
+    }
+  },
   components: {
-    AppLogo
   }
 }
 </script>
 
-<style>
-*, *:focus {
-  box-sizing: border-box;
-  outline: none;
-}
+<style lang="sass">
+*, *:focus
+  box-sizing: border-box
+  outline: none
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+img
+  width: 100%
+  height: auto
+  display: block
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+.container
+  width: 100%
+  min-height: 100vh
+  padding: 60px
+  .title
+    font-size: 60px
+    font-wieght: bold
+    color: #4fc08d
+    text-align: center
+  .profile
+    display: flex
+    flex-wrap: wrap
+    justify-content: flex-start
+    align-items: center
+    width: 100%
+    margin-top: 60px
+    &__list
+      width: 33%
+      &:nth-child(2n+2)
+        margin: 0 1%
+      a
+        text-decoration: none
+        color: #4fc08d
+        font-weight: bold
+        text-align: center
+      &__title
+        margin-top: 10px
+        font-size: 20px
 </style>
